c652f77dd85cab3c04eb6aed779778d9431ac146
TheOtherDave <davesweeris@mac.com>
2016-01-15 13:26:57 -0800

    Initial Commit

---
+++ Hello Git/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello Git
+//
+//  Created by David Sweeris on 2016/01/15.
+//  Copyright © 2016 David Sweeris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
